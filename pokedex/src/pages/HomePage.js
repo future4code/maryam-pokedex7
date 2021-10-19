e3c4a5fdddf4abb39895b7edc3b06920b8072f0f
magdielSilva <Magdiel Silva>
2021-10-19 18:28:33 -0300

    renderização dos cardPokedex na homePage

--- pokedex/src/pages/HomePage.js
+++ pokedex/src/pages/HomePage.js
@@ -1,12 +1,35 @@
-import React from "react";
-import Header from "../components/Header"
-import CardPokedex from "../components/CardPokedex"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "../components/Header";
+import Button from '@mui/material/Button';
+import { Container } from "../styled/Styled";
+import CardPokedex from "../components/CardPokedex";
 
 export const HomePage = () => {
-    return(
-        <div>
-            <Header/>
-            <CardPokedex/>
-        </div>
-    )
-}
+  const [pokemonsList, setPokemonsList] = useState([]);
+
+  useEffect(() => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=20&limit=20`;
+    axios
+      .get(url)
+      .then((res) => {
+        setPokemonsList(res.data.results);
+        console.log(res.data.results);
+      })
+      .catch((error) => {
+        console.log("erro na requisição", error);
+      });
+  }, []);
+
+  const listPokemons = pokemonsList.map((pokemon) => {
+    return <CardPokedex name={pokemon.name} />;
+  });
+
+  return (
+    <div>
+      <Header />
+      <Container>{listPokemons}</Container>
+      <Button variant={"contained"} >BUSCAR MAIS POKEMONS</Button>
+    </div>
+  );
+};
