a10fe103c8f4bba32a3f17c9703737c1ee7602aa
Silvio Ribeiro Dias Jr <silviordjr@outlook.com>
2021-10-22 16:52:59 -0300

    poke battle

--- pokedex/src/pages/BattlePage.js
+++ pokedex/src/pages/BattlePage.js
@@ -12,6 +12,7 @@ const BattleContainer = styledComponentsCjs.div `
     align-items: center;
     margin-top: 3rem;
     margin-bottom: 15rem;
+    min-height: 80vh;
 `
 
 const Battlefield = styledComponentsCjs.div `
@@ -52,14 +53,33 @@ const AlignPokeComputer = styledComponentsCjs.div `
     }
 `
 
+const ContainerResultado = styledComponentsCjs.div`
+    margin-left:15vw;
+    text-transform: capitalize
+`
+const ContainerPlacar = styledComponentsCjs.div`
+    margin-left:15vw;
+`
+
+const ContainerSecundario = styledComponentsCjs.div`
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 30vh
+`
+
 function BattlePage () {
     const [pokeUser, setPokeUser] = useState([])
     const [pokeComputer, setPokeComputer] = useState([])
+    const [flagNewBattle, setFlagNewBattle] = useState(false)
+    const [scoreUser, setScoreUser] = useState(0)
+    const [scoreComputer, setScoreComputer] = useState(0)
     const history = useHistory()
     const params = useParams()
     const url = `https://pokeapi.co/api/v2/pokemon/${params.name}`
 
     useEffect(() => {
+        console.log('oi')
         axios.get(url)
         .then((res) => {
             setPokeUser(res.data)
@@ -79,12 +99,106 @@ function BattlePage () {
             window.alert("Ocorreu um erro! Tente novamente.")
             history.push('/error')
         })
-    }, [])
+    }, [flagNewBattle])
+
+    const onClickNewBattle = () => {
+        let pontosUser = 0
+        let pontosComputer = 0
+
+        pokeUser.stats && pokeUser.stats.map((stat) => {
+            pontosUser = pontosUser + stat.base_stat
+        })
+
+        pokeComputer.stats && pokeComputer.stats.map((stat) => {
+            pontosComputer = pontosComputer + stat.base_stat
+        })
+
+        if (pontosUser > pontosComputer){
+            setScoreUser(scoreUser + 1)
+        } else if (pontosComputer > pontosUser){
+            setScoreComputer(scoreComputer + 1)
+        }
+
+        setFlagNewBattle(!flagNewBattle)
+    }
+    
+    const goToPokedex = () => {
+        let pontosUser = 0
+        let pontosComputer = 0
+
+        pokeUser.stats && pokeUser.stats.map((stat) => {
+            pontosUser = pontosUser + stat.base_stat
+        })
+
+        pokeComputer.stats && pokeComputer.stats.map((stat) => {
+            pontosComputer = pontosComputer + stat.base_stat
+        })
+
+        if (pontosUser > pontosComputer){
+            setScoreUser(scoreUser + 1)
+        } else if (pontosComputer > pontosUser){
+            setScoreComputer(scoreComputer + 1)
+        }
+
+        history.push('/pokedex')
+    }
+
+    const renderizaVencedeor = () => {
+        let pontosUser = 0
+        let pontosComputer = 0
+
+        pokeUser.stats && pokeUser.stats.map((stat) => {
+            pontosUser = pontosUser + stat.base_stat
+        })
+
+        pokeComputer.stats && pokeComputer.stats.map((stat) => {
+            pontosComputer = pontosComputer + stat.base_stat
+        })
+
+        if (pontosComputer > pontosUser){
+            // setScoreComputer(scoreComputer + 1)
+            return(
+                <div>
+                    <h3>{pokeComputer.name} venceu!</h3>
+                    <h5>Placar da Batalha:</h5>
+                    <h5>{pokeComputer.name} {pontosComputer} X {pontosUser} {pokeUser.name} </h5>
+                    <div>
+                        <button onClick={onClickNewBattle} >Nova batalha com {pokeUser.name}</button>
+                        <button onClick={goToPokedex} >Escolher novo Pokemon na Pokedex</button>
+                    </div>
+                </div>
+            )
+        } else if (pontosComputer < pontosUser){
+            // setScoreUser(scoreUser + 1)
+            return(
+                <div>
+                    <h3>{pokeUser.name} venceu!</h3>
+                    <h5>Placar da Batalha:</h5>
+                    <h5>{pokeComputer.name} {pontosComputer} X {pontosUser} {pokeUser.name} </h5>
+                    <div>
+                        <button onClick={onClickNewBattle} >Nova batalha com {pokeUser.name}</button>
+                        <button onClick={goToPokedex} >Escolher novo Pokemon na Pokedex</button>
+                    </div>
+                </div>
+            )
+        } else{
+            return(
+                <div>
+                    <h3>Empate!</h3>
+                    <h5>Placar da Batalha:</h5>
+                    <h5>{pokeComputer.name} {pontosComputer} X {pontosUser} {pokeUser.name} </h5>
+                    <div>
+                        <button onClick={onClickNewBattle} >Nova batalha com {pokeUser.name}</button>
+                        <button onClick={goToPokedex} >Escolher novo Pokemon na Pokedex</button>
+                    </div>
+                </div>
+            )
+        }
+    }
 
 
     return(
         <BattleContainer>
-
             <Battlefield>
                 <img src={Background} alt="Campo de batalha" />
                 </Battlefield>
@@ -102,6 +216,17 @@ function BattlePage () {
                 alt="Pokémon de frente" 
                 />)}
             </AlignPokeComputer>
+            <ContainerSecundario>
+
+                <ContainerPlacar>
+                    <h1>Placar:</h1>
+                    <h3>Jogador {scoreUser} X {scoreComputer} Computador</h3>
+                </ContainerPlacar>
+                <ContainerResultado>
+                    {pokeComputer.stats &&(renderizaVencedeor())}
+                </ContainerResultado>
+                
+            </ContainerSecundario>
         </BattleContainer>
     )
 }
